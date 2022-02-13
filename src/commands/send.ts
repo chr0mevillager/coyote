fc4150f857b7405791bdbd0f62886b48839d4f10
Chrome Villager <chr0mevillager@gmail.com>
2022-02-12 17:01:46 -0700

    added functionality so only admin can use /send

--- src/commands/send.ts
+++ src/commands/send.ts
@@ -6,6 +6,7 @@ import {
 	CacheType,
 	InteractionCollector,
 	MessageComponentInteraction,
+	GuildMember,
 } from "discord.js";
 import { CustomCommand } from "../exports/types";
 import { client } from "../exports/client";
@@ -119,6 +120,17 @@ let send: CustomCommand = {
 	},
 
 	async execute(interaction) {
+		if (!(interaction.member as GuildMember).permissions.has("ADMINISTRATOR")) {
+			await interaction.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor("#ed4245")
+						.setTitle("You need to be a server admin to use this command!")
+				],
+				ephemeral: true,
+			});
+			return;
+		}
 		//Blank Vars
 		let title: string = "\u200B";
 		let description: string = "\u200B";
@@ -343,6 +355,7 @@ let send: CustomCommand = {
 		}
 	},
 };
+
 export default send;
 
 // const listeners: Record<string, InteractionCollector<MessageComponentInteraction<CacheType>>> = {};
