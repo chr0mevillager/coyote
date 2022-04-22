c1b213da264ec633938e34c0c58bd78d62e9da26
Chrome Villager <chr0mevillager@gmail.com>
2022-04-21 18:03:54 -0600

    Added support for all unicode escape sequences

--- src/commands/send.ts
+++ src/commands/send.ts
@@ -72,7 +72,9 @@ let send: CustomCommand = {
 
 		if (title.length > 256) title = title.slice(0, 256);
 		if (description.length > 4000) description = description.slice(0, 4000);
-		description = description.replace("\\n", "\u000A");
+
+		description = JSON.parse('"' + description.replace(/"/g, '\\"') + '"')
+		//description = description.replace("\\n", "\u000A");
 
 		//Buttons
 		const buttonRow = (uuid: string) => new MessageActionRow()
