b61b0cdbf6ff34a16d4812ff5b5f2de70c3b52b6
Chrome Villager <chr0mevillager@gmail.com>
2022-05-05 18:41:28 -0600

    Added error protection for error logging

--- src/exports/error.ts
+++ src/exports/error.ts
@@ -3,23 +3,27 @@ import { client } from "./client";
 
 /** Logs a error in a chat log and notifies the user of a issue. */
 export default async function logMessage(error: string, errorLocation: string, interaction?) {
-	if (interaction) {
-		await interaction.reply({
-			ephemeral: true,
+	try {
+		if (interaction) {
+			await interaction.reply({
+				ephemeral: true,
+				embeds: [
+					new MessageEmbed()
+						.setColor("#2f3136")
+						.setTitle("An error has occured!")
+						.setDescription("The developers were notified of this error and it will be fixed shortly.")
+				],
+			});
+		}
+		await (client.channels.cache.find((channel) => (channel as any).id === "971927932790534204") as any).send({
 			embeds: [
 				new MessageEmbed()
 					.setColor("#2f3136")
-					.setTitle("An error has occured!")
-					.setDescription("The developers were notified of this error and it will be fixed shortly.")
+					.setTitle("An error has occured in the `" + errorLocation + "`!")
+					.setDescription("```" + error + "```")
 			],
 		});
+	} catch (error) {
+		console.error(error);
 	}
-	await (client.channels.cache.find((channel) => (channel as any).id === "971927932790534204") as any).send({
-		embeds: [
-			new MessageEmbed()
-				.setColor("#2f3136")
-				.setTitle("An error has occured in the `" + errorLocation + "`!")
-				.setDescription("```" + error + "```")
-		],
-	});
 }
