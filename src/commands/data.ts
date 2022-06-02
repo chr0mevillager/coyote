145d3708a8c90bb2b051fae8d25c8a17c89a4ee0
Chrome Villager <chr0mevillager@gmail.com>
2022-06-02 10:35:20 -0600

    Updated data command

--- src/commands/data.ts
+++ src/commands/data.ts
@@ -1,7 +1,8 @@
-import { MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
+import { MessageEmbed } from "discord.js";
 import * as activity from "../exports/activity";
 import { client } from "../exports/client";
 import { CustomCommand } from "../exports/types";
+import * as commandData from "../exports/data";
 
 let data: CustomCommand = {
 	data: {
@@ -13,15 +14,43 @@ let data: CustomCommand = {
 			embeds: [
 				new MessageEmbed()
 					.setColor("#389af0")
-					.setTitle("Embeds Bot")
+					.setTitle("Main")
 					.addFields(
 						{ name: "\u2800\nServer:", value: "\u2800" },
 						{ name: "`Ping:`", value: (client.ws.ping + "ms"), inline: true },
 						{ name: "`Online since:`", value: ("<t:" + Math.floor(client.readyAt.getTime() / 1000) + ">"), inline: true },
+
 						{ name: "\u2800\nBot:", value: "\u2800" },
 						{ name: "`Server Count:`", value: (client.guilds.cache.size + ""), inline: true },
 						{ name: "`Activity:`", value: ("Index: " + activity.activityIndex + "\nWill Rotate: " + activity.activityRotation), inline: true },
-					)
+					),
+				new MessageEmbed()
+					.setColor("#389af0")
+					.setTitle("Commands")
+					.addFields(
+						{ name: "\u2800\n\/send message", value: "\u2800" },
+						{ name: "`Command Uses:`", value: commandData.data.message.uses + "", inline: true },
+						{ name: "`Send Button Uses:`", value: commandData.data.message.buttonUses.send + "", inline: true },
+						{ name: "`Cancel Button Uses:`", value: commandData.data.message.buttonUses.cancel + "", inline: true },
+						{ name: "`Timeout:`", value: (commandData.data.message.uses - (commandData.data.message.buttonUses.send + commandData.data.message.buttonUses.cancel)) + "", inline: true },
+						{ name: "`Intuition Score:`", value: (commandData.data.message.buttonUses.send / commandData.data.message.uses).toFixed(4) + "", inline: true },
+						{ name: "`Popularity per Server:`", value: (commandData.data.message.buttonUses.send / client.guilds.cache.size).toFixed(4) + "", inline: true },
+
+						{ name: "\u2800\n\/send poll", value: "\u2800" },
+						{ name: "`Command Uses:`", value: commandData.data.poll.uses + "", inline: true },
+						{ name: "`Send Button Uses:`", value: commandData.data.poll.buttonUses.send + "", inline: true },
+						{ name: "`Cancel Button Uses:`", value: commandData.data.poll.buttonUses.cancel + "", inline: true },
+						{ name: "`Replies:`", value: commandData.data.poll.buttonUses.response + "", inline: true },
+						{ name: "`Timeout:`", value: (commandData.data.poll.uses - (commandData.data.poll.buttonUses.send + commandData.data.poll.buttonUses.cancel)) + "", inline: true },
+						{ name: "`Intuition Score:`", value: (commandData.data.poll.buttonUses.send / commandData.data.poll.uses).toFixed(4) + "", inline: true },
+						{ name: "`Popularity per Server:`", value: (commandData.data.poll.buttonUses.send / client.guilds.cache.size).toFixed(4) + "", inline: true },
+
+						{ name: "\u2800\n\/help", value: "\u2800" },
+						{ name: "`Command Uses:`", value: commandData.data.help.uses + "", inline: true },
+
+						{ name: "\u2800\n\/info", value: "\u2800" },
+						{ name: "`Command Uses:`", value: commandData.data.info.uses + "", inline: true },
+					),
 			],
 			ephemeral: true,
 		});
